3d31a4f0ccd4be37eeb2f3df27cafcaa7c0e6181
Karsten Temme <ktemme@gmail.com>
2009-07-24 18:12:40 -0700

    Draggable enabled, serializable not

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-   	$(".prefix > .control").bind("click", function(){
+	enablePrefixClick('');
+	enableSuffixClick('');
+	enableSortable();
+});
+
+function enableSortable() {
+	$("#parts").sortable({
+		handle: '.handle'
+	});
+	$("a#return").bind("click",function() {    
+      alert($('#parts').sortable('serialize'));  
+    });
+};
+
+function enablePrefixClick(limits) {
+	$(limits + ".prefix > .control").bind("click", function(){
 		if ($(this).text() == '+Prefix') {
 			$(this).text('-Prefix');
 		}
@@ -9,11 +24,10 @@ $(document).ready(function() {
 		var div = $(this).parent();		
 		div.find('.data').toggle();
 	});
-});
+};
 
-
-$(document).ready(function() {
-   	$(".suffix > .control").bind("click", function(){
+function enableSuffixClick(limits){
+	$(limits + ".suffix > .control").bind("click", function(){
 		if ($(this).text() == '+Suffix') {
 			$(this).text('-Suffix');
 		}
@@ -23,5 +37,44 @@ $(document).ready(function() {
 		var div = $(this).parent();		
 		div.find('.data').toggle();
 	});
-});
+};
 
+function addFormField() {
+	var id = document.getElementById("partsCount").value;
+	$("#parts").append(
+			"<div id='parts_"+id+"' class='part'> \
+				<div class='handle'><a href='#' onClick='removeFormField(\"#part" + id + "\");return false;'>Remove</a></div> \
+				<div class='content'> \
+								  	Name: <input type='text' name='parts["+id+"][name]' class='field name' /> \
+					<div class='prefix'> \
+						<div class = 'control'>+Prefix</div> \
+						<div class = 'data'>Prefix: \
+							<input type='text' name='parts["+id+"][prefix]' class='field additional_sequence' /> \
+						</div> \
+					</div> \
+					<div class='sequence'> \
+								  		Sequence: <input type='text' name='parts["+id+"][sequence]' class='field sequence'/> \
+					</div> \
+					<div class ='suffix'> \
+						<div class='control'>+Suffix</div> \
+						<div class='data'>Suffix: \
+							<input type='text' name='parts["+id+"][suffix]' class='field additional_sequence' size='50' />	\
+						</div> \
+					</div> \
+					<br/> \
+					RC: <input type='checkbox' name='parts["+id+"][rc]' /> \
+					Order: <input type='text' name='parts["+id+"][order]' /> \
+				</div> \
+			</div>"	
+		);
+
+	enablePrefixClick('#part'+id+' >');
+	enableSuffixClick('#part'+id+' >');
+
+	id = (id-1)+2;
+	document.getElementById("partsCount").value = id;
+};
+
+function removeFormField(id) {
+	$(id).remove();
+};
